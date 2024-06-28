ace54fbd5b885854b9bf88282264f21d92f4658a
BitBelle <wainainamercy432@gmail.com>
2024-06-28 16:54:00 +0300

    worked on the ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserprofileComponent } from "./components/userprofile/userprofile.component";
+import { UserpostsComponent } from './components/userposts/userposts.component';
+import { PostcommentComponent } from './components/postcomment/postcomment.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, UserprofileComponent, UserpostsComponent, PostcommentComponent]
 })
 export class AppComponent {
   title = 'twitter_clone';
